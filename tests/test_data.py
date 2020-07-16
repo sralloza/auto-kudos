8bc36c501933fb713ecc7f2352bf43e22363d51c
sralloza <sralloza@gmail.com>
2020-07-16 10:35:40 +0200

    test: cover give_kudos_to_everyone

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 from unittest import mock
 
-import pytest
-
 from strava_api.activity import Activity
-from strava_api.data import get_activities
+from strava_api.data import get_activities, give_kudos_to_everyone
 
 
 @mock.patch("strava_api.data.session.get")
@@ -16,3 +14,13 @@ def test_get_activities(get_m):
     for i, act in enumerate(activities):
         i += 1
         assert act == Activity(f"Owner-{i}", i, f"Activity-{i}", i != 19)
+
+
+@mock.patch("strava_api.data.get_activities")
+def test_give_kudos_to_everyone(get_acts_m):
+    mymock = mock.MagicMock()
+    get_acts_m.return_value = [mymock] * 15
+
+    give_kudos_to_everyone()
+    mymock.ensure_kudo.assert_called_with()
+    assert mymock.ensure_kudo.call_count == 15
