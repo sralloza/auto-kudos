72c4332afe0795914b3898dc80f9aeb6f2cb75ba
sralloza <sralloza@gmail.com>
2020-07-16 09:23:34 +0200

    refactor: rename launch.py to launcher.py

---
+++ launcher.py
@@ -0,0 +1,18 @@
+import logging
+from pathlib import Path
+
+from strava_api.data import give_kudos_to_everyone
+
+fmt = "[%(asctime)s] %(levelname)s - %(name)s:%(lineno)s - %(message)s"
+filename = Path(__file__).with_name("auto-kudos.log")
+handlers = [logging.FileHandler(filename, "at", "utf-8")]
+logging.basicConfig(level=10, format=fmt, handlers=handlers)
+logging.getLogger("urllib3.connectionpool").setLevel(60)
+
+
+def main():
+    give_kudos_to_everyone()
+
+
+if __name__ == "__main__":
+    main()
