45b70ebfb2848fe8fd16e8cc504bb4cdff638e2d
sralloza <sralloza@gmail.com>
2020-07-22 13:36:02 +0200

    feat: add retry control to Session

--- strava_api/networking.py
+++ strava_api/networking.py
@@ -3,7 +3,7 @@
 from logging import getLogger
 
 from bs4 import BeautifulSoup
-from requests import Session as ReqSession
+from requests import ConnectionError as ReqConnectionError, Session as ReqSession
 
 from .credentials import credentials
 from .exceptions import LoginError
@@ -17,16 +17,18 @@ class Session(ReqSession):
         self.headers.update({"user-agent": "strava-py"})
 
         self.logger = getLogger(__name__)
+        self.retries = 5
         self.primary_url = "https://www.strava.com/dashboard"
         self.login_url = "https://www.strava.com/session"
         self.data_url = "https://www.strava.com/dashboard/following/1000"
-
-        self.login()
+        self.logged_in = False
+        self.logging_in = False
 
     def login(self):
         """Logs into the strava website."""
 
         self.logger.debug("Logging in")
+        self.logging_in = True
         response = self.get(self.primary_url)
         soup = BeautifulSoup(response.text, "html.parser")
         utf8 = soup.find("input", {"name": "utf8"})["value"]
@@ -46,6 +48,21 @@ class Session(ReqSession):
 
         self.headers.update({"x-csrf-token": token})
         self.logger.debug("Logged in")
+        self.logged_in = True
+        self.logging_in = False
+
+    def request(self, *args, **kwargs):
+        if not self.logged_in and not self.logging_in:
+            self.login()
+
+        retries = self.retries
+        while retries:
+            try:
+                return super().request(*args, **kwargs)
+            except ReqConnectionError as exc:
+                retries -= 1
+                if not retries:
+                    raise ConnectionError from exc
 
 
 session = Session()
