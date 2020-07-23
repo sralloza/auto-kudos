5e6bf03ad8606f10bd098ff825c588ff5089721e
sralloza <sralloza@gmail.com>
2020-07-23 17:29:20 +0200

    feat: add requirements and packages to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 """Install script designed only for testing."""
 
+from pathlib import Path
+
 from setuptools import setup
 
-setup(name="auto-kudos", version="1.0")
+reqs = Path(__file__).with_name("requirements.txt").read_text().splitlines()
+setup(name="auto-kudos", version="1.0", install_requires=reqs, packages=["strava_api"])
