a6d52bf761692a752bbc66bbc6af9cb6f7de586b
Diego Alloza González <sralloza@gmail.com>
2021-08-31 09:13:18 +0200

    chore: fix aws.py according to pylint

--- strava_api/aws.py
+++ strava_api/aws.py
@@ -1,3 +1,5 @@
+"""Interface with AWS Lambda."""
+
 import logging
 import os
 from json import loads
@@ -5,7 +7,16 @@ from json import loads
 from strava_api.data import give_kudos_to_everyone
 
 
-def aws_lambda(event: str, lambda_context):
+def aws_lambda(event: str, lambda_context: object):
+    """Lambda handler.
+
+    Args:
+        event (str): event information
+        lambda_context (object): lambda context
+
+    Raises:
+        TypeError: if event is neither JSON string nor dict.
+    """
     logger = logging.getLogger(__name__)
     try:
         if isinstance(event, str):
@@ -18,7 +29,7 @@ def aws_lambda(event: str, lambda_context):
             os.environ["STRAVA_USERNAME"] = data["username"]
         if "password" in data:
             os.environ["STRAVA_PASSWORD"] = data["password"]
-    except Exception:
-        logger.exception("Error loading event")
+    except Exception:  # pylint: disable=broad-except
+        logger.exception("Error loading event (context=%s)", lambda_context)
 
     give_kudos_to_everyone()
